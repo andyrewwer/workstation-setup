87f701b9ba54f1977cff64b0439708664286f1bb
andyrewwer <andrew.weeks@me.com>
2021-12-17 12:40:37 -0800

    Create andrew-custom.sh

---
+++ scripts/common/andrew-custom.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# All these applications are independent, so if one
+# fails to install, don't stop.
+set +e
+
+echo
+echo "Installing utilities for Andrew"
+brew install --cask rocket
+brew install --cask keyboard-maestro
+brew install --cask krisp
+brew install --cask karabiner-elements
+brew install --cask choosy
+brew install --cask launchbar
+
+echo "Done installing utilities for Andrew"
+
+set -e 
