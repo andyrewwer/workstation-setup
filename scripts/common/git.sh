ec03b8af8ba8086339bc6afc690607d0ad2aef43
Andrew Weeks <andrew.weeks@me.com>
2023-06-10 08:38:08 -0700

    Update git.sh

--- scripts/common/git.sh
+++ scripts/common/git.sh
@@ -20,10 +20,3 @@ echo "export GIT_DUET_ROTATE_AUTHOR=true" >> ~/.zshenv  # rotate the author
 echo "export GIT_DUET_SET_GIT_USER_CONFIG=1" >> ~/.zshenv  # use regular git commands
 echo "Putting a sample authors file in ~/.git-authors if it isn't already there"
 cp -n files/.git-authors ~/.git-authors || true
-
-echo "Installing git UI tools"
-set +e # Optional; don't exit if they fail
-brew install --cask rowanj-gitx
-brew install --cask sourcetree
-brew install --cask gitup
-set -e
